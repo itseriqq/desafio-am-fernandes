45af5da636b218364adfac2801c2f1dfd40cb743
Erick <contato.bioerick@gmail.com>
2024-08-14 17:01:24 -0300

    Hover effect changed to navbar items

--- src/app/components/Navbar/Navbar.jsx
+++ src/app/components/Navbar/Navbar.jsx
@@ -23,10 +23,10 @@ const Navbar = () => {
             </div>
             {/* inicio itens padrao */}
             <div className={styles.navItens} id="">
-                <Link href='#' className={`link ${styles.dNoneLg}`}>Início</Link>
-                <Link href='#' className={`link ${styles.dNoneLg}`}>Imóveis</Link>
-                <Link href='#' className={`link ${styles.dNoneLg}`}>Contato</Link>
-                <Link href='#' className={`link ${styles.dNoneLg}`}>Localização</Link>
+                <Link href='#' className={`${styles.linkNav} ${styles.dNoneLg}`}>Início</Link>
+                <Link href='#' className={`${styles.linkNav} ${styles.dNoneLg}`}>Imóveis</Link>
+                <Link href='#' className={`${styles.linkNav} ${styles.dNoneLg}`}>Contato</Link>
+                <Link href='#' className={`${styles.linkNav} ${styles.dNoneLg}`}>Localização</Link>
             </div>
             <button className={`botaogeral ${styles.dNoneLg}`} type="button">Entrar</button>
 
@@ -35,10 +35,10 @@ const Navbar = () => {
             {/* inicio itens sm-devices */}
     
             <div className={styles.navItensSm} id="">
-                <Link href='#' className={`link ${styles.dNoneSm} ${open ? '' : styles.dNone}`}>Início</Link>
-                <Link href='#' className={`link ${styles.dNoneSm} ${open ? '' : styles.dNone}`}>Imóveis</Link>
-                <Link href='#' className={`link ${styles.dNoneSm} ${open ? '' : styles.dNone}`}>Contato</Link>
-                <Link href='#' className={`link ${styles.dNoneSm} ${open ? '' : styles.dNone}`}>Localização</Link>
+                <Link href='#' className={`${styles.linkNav} ${styles.dNoneSm} ${open ? '' : styles.dNone}`}>Início</Link>
+                <Link href='#' className={`${styles.linkNav} ${styles.dNoneSm} ${open ? '' : styles.dNone}`}>Imóveis</Link>
+                <Link href='#' className={`${styles.linkNav} ${styles.dNoneSm} ${open ? '' : styles.dNone}`}>Contato</Link>
+                <Link href='#' className={`${styles.linkNav} ${styles.dNoneSm} ${open ? '' : styles.dNone}`}>Localização</Link>
             <button className={`botaogeral ${open ? '' : styles.dNone}`} type="button">Entrar</button>
             </div>
 
