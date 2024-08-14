e2cfac1124a6875fedfa66de7d846f6f00d14889
Erick <contato.bioerick@gmail.com>
2024-08-14 15:25:18 -0300

    Fetching buildings informations from API

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,12 +1,49 @@
+'use client'
+import React, { useEffect, useState } from "react";
 import styles from "./page.module.css";
+import axios from 'axios';
 
 export default function Home() {
+
+  const [imoveis, setImoveis] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const FetchImoveis = async () => {
+      try {
+        const res = await axios.get(`https://api.estagio.amfernandes.com.br/imoveis`);
+        setImoveis(res.data);
+        console.log(res);
+      } catch (error) {
+        setError(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+    FetchImoveis();
+  }, []);
+  
+
   return (
-    <>
-    <div className="container">
-      <h1 className="text-center my-4">Olá, Bootstrap!</h1>
-      <button className="btn btn-primary">Clique aqui</button>
+    <div>
+      {loading ? (
+        <p>Carregando...</p>
+      ) : error ? (
+        <p>Erro ao carregar imóveis</p>
+      ) : (
+        <div>
+          {imoveis.map((imovel, index) => (
+            <div key={index}>
+              <h2>{imovel.nome}</h2>
+              <p>{imovel.rua}, {imovel.num}, {imovel.bairro}</p>
+              <p>{imovel.cidade} - {imovel.cep}</p>
+              <p>Preço: R$ {imovel.planta.preco}</p>
+              <img src={imovel.fachada} alt={`Fachada do ${imovel.nome}`} />
+            </div>
+          ))}
+        </div>
+      )}
     </div>
-    </>
   );
 }
