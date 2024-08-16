264bcc7b5fe9506600b4fcddcd23edb700fd2f94
Erick <contato.bioerick@gmail.com>
2024-08-15 23:02:11 -0300

    Bug of key of each imovelcard fixed, filters are now working

--- src/app/imoveis/page.jsx
+++ src/app/imoveis/page.jsx
@@ -35,28 +35,62 @@ export default function ImoveisPage() {
       ...filters,
       [e.target.name]: e.target.value
     });
+    getFilteredImoveis()
   };
 
-  const filteredImoveis = imoveis.filter(imovel => {
-    return (!filters.localizacao || imovel.rua.toLowerCase().includes(filters.localizacao.toLowerCase())) &&
-      (!filters.quartos || imovel.planta.dorms === parseInt(filters.quartos)) &&
-      (!filters.vagas || imovel.planta.vagas === parseInt(filters.vagas)) &&
-      (!filters.preco || (filters.preco === 'ordemMenorPreco' ? imovel.planta.preco <= filters.precoValue : imovel.planta.preco >= filters.precoValue)) &&
-      (!filters.bairro || imovel.bairro.toLowerCase().includes(filters.bairro.toLowerCase()));
-  });
+  const getFilteredImoveis = () => {
+    return imoveis
+      .filter(imovel => {
+        const localizacaoMatch = !filters.localizacao ||
+          (imovel.rua.toLowerCase().includes(filters.localizacao.toLowerCase()) ||
+            imovel.bairro.toLowerCase().includes(filters.localizacao.toLowerCase()) ||
+            imovel.cidade.toLowerCase().includes(filters.localizacao.toLowerCase()) ||
+            imovel.cep.toLowerCase().includes(filters.localizacao.toLowerCase()));
+
+        const quartosMatch = !filters.quartos || imovel.planta.dorms === parseInt(filters.quartos);
+        const vagasMatch = !filters.vagas || imovel.planta.vagas === parseInt(filters.vagas);
+        const bairroMatch = !filters.bairro || imovel.bairro.toLowerCase().includes(filters.bairro.toLowerCase());
+
+        return localizacaoMatch && quartosMatch && vagasMatch && bairroMatch;
+      })
+      .sort((a, b) => {
+        if (filters.preco === 'ordemMenorPreco') {
+          return a.planta.preco - b.planta.preco;
+        } else if (filters.preco === 'ordemMaiorPreco') {
+          return b.planta.preco - a.planta.preco;
+        } else {
+          return 0;
+        }
+      });
+  };
+
+  const filteredImoveis = getFilteredImoveis();
 
   return (
     <>
       <div className={styles.filterBar}>
         <div className={styles.input}>
-          <label htmlFor="text">Busque por Localização</label>
-          <input type="text" name="localizacao" placeholder="Ex.: São Paulo" />
+          <label htmlFor="localizacao">Busque por Localização</label>
+          <input
+            type="text"
+            name="localizacao"
+            id="localizacao"
+            placeholder="Ex.: São Paulo"
+            value={filters.localizacao}
+            onChange={handleFilterChange}
+          />
         </div>
-
 
         <div className={styles.input}>
           <label htmlFor="quartos">Quartos</label>
-          <select id="quartos" className={styles.select} name="quartos" value={1} >
+          <select
+            id="quartos"
+            name="quartos"
+            className={styles.select}
+            value={filters.quartos}
+            onChange={handleFilterChange}
+          >
+            <option value="">Todos</option>
             <option value="1">1 quarto</option>
             <option value="2">2 quartos</option>
             <option value="3">3 quartos</option>
@@ -66,8 +100,15 @@ export default function ImoveisPage() {
 
         <div className={styles.input}>
           <label htmlFor="vagas">Vagas</label>
-          <select id="vagas" className={styles.select} name="vagas" value={1} >
-            <option value="1">1 quarto</option>
+          <select
+            id="vagas"
+            name="vagas"
+            className={styles.select}
+            value={filters.vagas}
+            onChange={handleFilterChange}
+          >
+            <option value="">Todas</option>
+            <option value="1">1 vaga</option>
             <option value="2">2 vagas</option>
             <option value="3">3 vagas</option>
             <option value="4">4 vagas</option>
@@ -75,27 +116,37 @@ export default function ImoveisPage() {
         </div>
 
         <div className={styles.input}>
-          <label htmlFor="ordenacao">Preço</label>
-          <select className={styles.select} id="ordenacao" name="ordenacao" >
-            <option value="ordemMaiorPreco">Maior preço</option>
+          <label htmlFor="preco">Preço</label>
+          <select
+            id="preco"
+            name="preco"
+            className={styles.select}
+            value={filters.preco}
+            onChange={handleFilterChange}
+          >
+            <option value="">Ordenar por preço</option>
             <option value="ordemMenorPreco">Menor preço</option>
+            <option value="ordemMaiorPreco">Maior preço</option>
           </select>
         </div>
 
         <div className={styles.input}>
-          <label htmlFor="ordenacao">Bairro</label>
-          <select className={styles.select} id="ordenacao" name="ordenacao" >
-            <option value="ordemMaiorPreco">Maior preço</option>
-            <option value="ordemMenorPreco">Menor preço</option>
-          </select>
+          <label htmlFor="bairro">Bairro</label>
+          <input
+            type="text"
+            name="bairro"
+            id="bairro"
+            placeholder="Ex.: Vila Gilda"
+            value={filters.bairro}
+            onChange={handleFilterChange}
+          />
         </div>
-
       </div>
 
       <div className={styles.imoveisContainer}>
         {loading ? <p>Carregando os imóveis...</p> : error ? <p>Erro no carregamento</p> : (
-          filteredImoveis.map(imovel => (
-            <ImovelCard key={imovel.cep} imovel={imovel} />
+          filteredImoveis.map((imovel, index) => (
+            <ImovelCard key={index} imovel={imovel} />
           ))
         )}
       </div>
