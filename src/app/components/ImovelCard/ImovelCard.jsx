2cecfdd2695d92deccada86de00936db83a3f2f6
Erick <contato.bioerick@gmail.com>
2024-08-15 21:33:00 -0300

    Imovel card done

--- src/app/components/ImovelCard/ImovelCard.jsx
+++ src/app/components/ImovelCard/ImovelCard.jsx
@@ -6,29 +6,38 @@ import styles from "./imovelcard.module.css"
 
 const ImovelCard = ({ imovel }) => (
   <div className={styles.card}>
+
+    {/* inicio tag */}
+
+    <div className={styles.tag}>
+      <span>Exclusivo</span>
+    </div>
+    {/* fim tag */}
+
     {/* imagem */}
     <div className={styles.img}>
       <Image src="https://firebasestorage.googleapis.com/v0/b/easyhouse-amf.appspot.com/o/taste.png?alt=media" width={500} height={500}></Image>
     </div>
     {/* fim da imagem */}
 
+    {/* inicio descricao */}
     <div className={styles.desc}>
       <span className={styles.categoria}>Apartamento</span>
       <h2 className={styles.preco}>R$ 1.200.000</h2>
-      <div className={styles.metragemDorms}>
-        <p>
-          <BedIcon />
-          3 dormitórios
-        </p>
-        <p>
-          75m²
-        </p>
+      <div className={styles.MetragemDormsVagas}>
+        <span>3 quartos</span>
+        <span>•</span>
+        <span>2 vagas</span>
+        <span>•</span>
+        <span>
+          75 m²
+        </span>
       </div>
-      <p>
-        <LocationOnIcon />
-        Santo André, 124
-      </p>
+      <div className={styles.local}>
+        Rua Kugler, Vila Gilda - Santo André
+      </div>
     </div>
+    {/* fim descricao */}
   </div>
 );
 
