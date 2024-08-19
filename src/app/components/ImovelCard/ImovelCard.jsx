62233aee7c2f30adde6904343cccf0d857005625
Erick <erickaug13@hotmail.com>
2024-08-18 22:27:51 -0300

    Dynamic pages are now working to each building using _lat & _long informations

--- src/app/components/ImovelCard/ImovelCard.jsx
+++ src/app/components/ImovelCard/ImovelCard.jsx
@@ -1,44 +1,66 @@
 import React from "react";
 import Image from "next/image";
 import styles from "./imovelcard.module.css";
-import GlobalStyles from "@/app/globals.css"
+import GlobalStyles from "@/app/globals.css";
 import Link from "next/link";
 
-const ImovelCard = ({ imovel }) => (
-  <Link href="/imoveis/1" className={`link ${styles.card}`}>
-    {/* início tag */}
-    <div className={styles.tag}>
-      <span>Exclusivo</span>
-    </div>
-    {/* fim tag */}
+// example of how we could build an unique id for each building
+// actually, the use of this function is not thaaaat optimized as we would need to
+// get through the whole building's array, using this method, to find which
+// building should be displayed. It takes lots of performance away.
+// anyway, i'd like to show it as an idea and implement it 
 
-    {/* imagem */}
-    <div className={styles.img}>
-      <Image src={imovel.fachada} width={500} height={300} alt={imovel.nome} />
-    </div>
-    {/* fim da imagem */}
+function generateImovelSlug(latitude, longitude) {
+  const latAbs = Math.abs(latitude).toString().replace('.', '');
+  const longAbs = Math.abs(longitude).toString().replace('.', '');
 
-    {/* início descricao */}
-    <div className={styles.desc}>
-      <span className={styles.categoria}>Apartamento</span>
-      <h2 className={styles.preco}>R$ {imovel.planta.preco.toLocaleString()}</h2>
-      <div className={styles.MetragemDormsVagas}>
-        <span>{imovel.planta.dorms} quartos</span>
-        {imovel.planta.vagas !== null && (
-          <>
-            <span>•</span>
-            <span>{imovel.planta.vagas} vagas</span>
-          </>
-        )}
-        <span>•</span>
-        <span>{imovel.planta.metragem} m²</span>
+  if (latAbs > longAbs) {
+    return `${latAbs}${longAbs}`;
+  } else {
+    return `${longAbs}${latAbs}`;
+  }
+}
+
+const ImovelCard = ({ imovel }) => {
+
+  const slug = generateImovelSlug(imovel.location._lat, imovel.location._long);
+
+  return (
+    <Link href={`/imoveis/${slug}`} className={`link ${styles.card}`}>
+      {/* início tag */}
+      <div className={styles.tag}>
+        <span>Exclusivo</span>
       </div>
-      <div className={styles.local}>
-        {imovel.rua}, {imovel.bairro} - {imovel.cidade}
+      {/* fim tag */}
+
+      {/* imagem */}
+      <div className={styles.img}>
+        <Image src={imovel.fachada} width={500} height={300} alt={imovel.nome} />
       </div>
-    </div>
-    {/* fim descricao */}
-  </Link>
-);
+      {/* fim da imagem */}
+
+      {/* início descricao */}
+      <div className={styles.desc}>
+        <span className={styles.categoria}>Apartamento</span>
+        <h2 className={styles.preco}>R$ {imovel.planta.preco.toLocaleString()}</h2>
+        <div className={styles.MetragemDormsVagas}>
+          <span>{imovel.planta.dorms} quartos</span>
+          {imovel.planta.vagas !== null && (
+            <>
+              <span>•</span>
+              <span>{imovel.planta.vagas} vagas</span>
+            </>
+          )}
+          <span>•</span>
+          <span>{imovel.planta.metragem} m²</span>
+        </div>
+        <div className={styles.local}>
+          {imovel.rua}, {imovel.bairro} - {imovel.cidade}
+        </div>
+      </div>
+      {/* fim descricao */}
+    </Link>
+  );
+};
 
 export default ImovelCard;
--- src/app/imoveis/[slug]/page.jsx
+++ src/app/imoveis/[slug]/page.jsx
@@ -1,38 +1,83 @@
 'use client'
 
-import React, { useState } from 'react'
-import styles from "./imovel.module.css"
-import Image from 'next/image'
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import styles from "./imovel.module.css";
 import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
+import { useParams, useRouter } from 'next/navigation';
 
 const ImovelPage = () => {
     const [currentImg, setCurrentImg] = useState(0);
+    const [imovel, setImovel] = useState(null);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+    const router = useRouter();
+    const { slug } = useParams(); 
 
-    const DATA = [
-        { image: 'https://firebasestorage.googleapis.com/v0/b/easyhouse-amf.appspot.com/o/taste.png?alt=media' },
-        { image: 'https://firebasestorage.googleapis.com/v0/b/easyhouse-amf.appspot.com/o/sajardimilhamykonos.jpg?alt=media' },
-        { image: 'https://firebasestorage.googleapis.com/v0/b/easyhouse-amf.appspot.com/o/sajardimjarina.jpg?alt=media' },
-    ];
+    useEffect(() => {
+        const fetchImoveis = async () => {
+            if (!slug) return;
+
+            try {
+                const res = await axios.get('https://api.estagio.amfernandes.com.br/imoveis');
+                const imoveis = res.data;
+
+                let targetImovel = null;
+                for (const imovel of imoveis) {
+                    const latAbs = Math.abs(imovel.location._lat).toString().replace('.', '');
+                    const longAbs = Math.abs(imovel.location._long).toString().replace('.', '');
+                    const imovelSlug = latAbs > longAbs ? `${latAbs}${longAbs}` : `${longAbs}${latAbs}`;
+
+                    if (imovelSlug === slug) {
+                        targetImovel = imovel;
+                        break;
+                    }
+                }
+
+                setImovel(targetImovel);
+            } catch (error) {
+                setError(error);
+            } finally {
+                setLoading(false);
+            }
+        };
+
+        fetchImoveis();
+    }, [slug]);
 
     const handleImageChangeBackward = () => {
         setCurrentImg(prevImg => (prevImg - 1 + DATA.length) % DATA.length);
-    }
+    };
 
     const handleImageChangeForward = () => {
         setCurrentImg(prevImg => (prevImg + 1) % DATA.length);
-    }
+    };
+
+    if (loading) return <p style={{marginTop: '40px', marginBottom: '40px'}}>Carregando...</p>;
+    if (error) return <p>Erro ao carregar os imóveis: {error.message}</p>;
+    if (!imovel) return <p>Não foi encontrado nenhum imóvel.</p>; 
+
+    const DATA = [
+        { image: imovel.fachada },
+        { image: 'https://firebasestorage.googleapis.com/v0/b/easyhouse-amf.appspot.com/o/sajardimilhamykonos.jpg?alt=media' },
+        { image: 'https://firebasestorage.googleapis.com/v0/b/easyhouse-amf.appspot.com/o/sajardimjarina.jpg?alt=media' },
+    ];
 
     return (
         <div className={`${styles.container}`}>
             <div className={`${styles.inicial}`}>
                 <div className={`${styles.header}`}>
-                    <h1>Apartamento para comprar com 70m², 2 quartos e 1 vaga</h1>
+                    <h1>{imovel.nome} com {imovel.planta.dorms} quartos, {imovel.planta.metragem} m² {imovel.planta.vagas !== null && (
+                            <>
+                                e {imovel.planta.vagas} vagas
+                            </>
+                        )}</h1>
 
                     <div className={styles.headerLegenda}>
                         <div className={styles.tag}>Exclusivo</div>
                         <div className={styles.tag}>Novo</div>
-                        <h3 className={styles.headerLegendaTotal}>Total R$ 254.000</h3>
+                        <h3 className={styles.headerLegendaTotal}>Total R$ {imovel.planta.preco.toLocaleString()}</h3>
                         <h3 className={styles.headerLegendaFinanciamento}>Aceita financiamento</h3>
 
                         <div className={styles.divBotao}>
@@ -40,8 +85,8 @@ const ImovelPage = () => {
                             <button className={styles.botao}>Negociar</button>
                         </div>
                     </div>
-
                 </div>
+
                 <div className={`${styles.imagem}`}>
                     <div className={styles.botoes}>
                         <div className={styles.botaoEsq} onClick={handleImageChangeBackward}>
@@ -53,9 +98,10 @@ const ImovelPage = () => {
                     </div>
                     <img src={DATA[currentImg].image} alt="Imagem do imóvel" />
                 </div>
+                
             </div>
         </div>
-    )
-}
+    );
+};
 
 export default ImovelPage;
